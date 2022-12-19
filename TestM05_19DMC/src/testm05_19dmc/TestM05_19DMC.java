fd5d077f3612919e2870588e9acaca71ba5dbfe1
ausias <ausias@hINFO6-23.xiam.cat>
2022-12-19 11:42:39 +0100

    Primer commit

---
+++ TestM05_19DMC/src/testm05_19dmc/TestM05_19DMC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testm05_19dmc;
+
+/**
+ *
+ * @author YbarTom
+ */
+public class TestM05_19DMC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
